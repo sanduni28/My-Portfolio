59a281b8ff4c6d82cdc51b0670cb19c953493e3c
sanduni28 <s22010154@ousl.lk>
2025-06-14 21:57:47 +0530

    Config change port

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/My-Portfolio/',
   plugins: [react()],
   server: {
-    port: 5173
+    port: 45000,
   }
 })
 
